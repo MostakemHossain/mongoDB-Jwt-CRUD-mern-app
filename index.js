31821e056b8ff676fa14cb472670fc1a81d88567
MostakemHossain <mostakememon123@gmail.com>
2023-12-06 21:25:20 +0600

    connecting to mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const express = require('express');
-const app= express();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
+mongoose
+  .connect(
+    `mongodb+srv://user-auth:ZsFA9bMGArevkGxU@cluster0.do2y2gc.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(
+    console.log("Connecting to mongoDB")
+  )
+  .catch((err) => {
+    console.log("Error Connecting to mongoDB");
+  });
 
-app.listen(port,()=>{
-    console.log(`Server is running on ${port}`);
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
